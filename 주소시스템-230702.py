f626aec717ebe33165d80044abb1ba4fe6b72fe1
acskang <acskang@gmail.com>
2023-07-09 11:52:43 +0900

    animateSVGwithCSS

---
+++ 주소시스템-230702.py
@@ -0,0 +1,20 @@
+addr_kr = [
+    "서울특별시 강남구 테헤란로 123번길 45, 가산빌딩",
+    "경기도 성남시 분당구 판교로 456번길 78, 테크노타워",
+    "대구광역시 동구 신천동 789-10, 코리아타워",
+    "부산광역시 해운대구 해운대로 1111, 더베이호텔",
+    "인천광역시 연수구 컨벤시아대로 222, 인천컨벤션센터",
+]
+
+addr_kr = [
+    { "ch": "中国广东省深圳市南山区科技园北区1号",
+    "en": "1 North Technology Park, Nanshan District, Shenzhen, Guangdong Province, China" },
+    { "ch": "中国上海市浦东新区世纪大道100号",
+    "en": "100 Century Avenue, Pudong New Area, Shanghai, China" },
+    { "ch": "中国北京市朝阳区建国门外大街1号",
+    "en": "1 Jianguomenwai Avenue, Chaoyang District, Beijing, China" },
+    { "ch": "中国广东省广州市天河区体育东路123号",
+    "en": "123 Tiyu East Road, Tianhe District, Guangzhou, Guangdong Province, China" },
+    { "ch": "中国四川省成都市锦江区春熙路456号",
+    "en": "456 Chunxi Road, Jinjiang District, Chengdu, Sichuan Province, China" }
+]
